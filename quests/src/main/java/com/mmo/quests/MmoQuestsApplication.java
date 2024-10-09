02a681d8cbf9a31885a33290628d5343433f8f48
kkdipes <kauanmp7@gmail.com>
2024-10-09 15:00:25 -0300

    projeto avaliação 2(mmoquests)

---
+++ quests/src/main/java/com/mmo/quests/MmoQuestsApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.quests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoQuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoQuestsApplication.class, args);
+	}
+
+}
---
+++ quests/src/test/java/com/mmo/quests/MmoQuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.quests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoQuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
